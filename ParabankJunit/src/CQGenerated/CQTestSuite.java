5937c816aeb30748e8df4da201c461089cce7dc6
vijaya <k.vijayarani225@gmail.com>
2018-09-06 13:35:57 +0530

    Commit4
    Commit4

--- ParabankJunit/src/CQGenerated/CQTestSuite.java
+++ ParabankJunit/src/CQGenerated/CQTestSuite.java
@@ -9,9 +9,9 @@ import CQGenerated.testsuite.*;
 @Suite.SuiteClasses({
 	TestCase1.class,
 	TestCase2.class,
-	/*TestCase3.class,
+	TestCase3.class,
 	TestCase4.class,
-	TestCase5.class,
+	/*TestCase5.class,
 	TestCase6.class*/
 	TestCase7.class,
 	/*TestCase8.class,
